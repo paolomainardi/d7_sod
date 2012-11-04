8ccaac9a63f57f3cda02e3b87d80edc6978bdcde
Paolo Mainardi (twinbit) <paolo@twinbit.it>
2012-11-04 22:31:46 +0200

    memcache production config

--- sites/sod/settings.development.php
+++ sites/sod/settings.development.php
@@ -501,3 +501,8 @@ $conf['404_fast_html'] = '<html xmlns="http://www.w3.org/1999/xhtml"><head><titl
  * Remove the leading hash signs to disable.
  */
 # $conf['allow_authorize_operations'] = FALSE;
+/* Memcache configuration */
+$conf['cache_backends'][] = 'sites/all/modules/contrib/memcache/memcache.inc';
+$conf['cache_default_class'] = 'MemCacheDrupal';
+$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
+$conf['memcache_key_prefix'] = 'sod_production';
