1325d89255f6f79c6e42ddc43d17e24bae31bee0
Paolo Mainardi <paolo@twinbit.it>
2014-02-16 22:24:09 +0100

    upgrade features

--- sites/all/themes/sod_bootstrap/templates/node.tpl.php
+++ sites/all/themes/sod_bootstrap/templates/node.tpl.php
@@ -15,11 +15,11 @@
       </span>
     <?php endif; ?>
 
-    <?php if ($page) : ?>
+    <?php if ($page && ($node->type !== 'page')) : ?>
       <!-- AddThis Button BEGIN -->
       <div class="addthis_toolbox addthis_default_style" style="margin-top: 10px;">
       <a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
-      <a class="addthis_button_google_plusone" g:plusone:annotation="bubble"></a> 
+      <a class="addthis_button_google_plusone" g:plusone:annotation="bubble"></a>
       <a class="addthis_button_tweet"></a>
       <a class="addthis_button_pinterest_pinit"></a>
       <a class="addthis_counter addthis_pill_style"></a>
@@ -27,7 +27,7 @@
       <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
       <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js"></script>
       <!-- AddThis Button END -->
-    <?php endif; ?> 
+    <?php endif; ?>
 
 
   </header>
