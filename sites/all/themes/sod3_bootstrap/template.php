9efd4344291fca9fa8e226632a3a8bfcd8d6ce72
Paolo Mainardi <paolo@twinbit.it>
2014-02-23 22:31:19 +0100

    doc

--- sites/all/themes/sod3_bootstrap/template.php
+++ sites/all/themes/sod3_bootstrap/template.php
@@ -5,6 +5,10 @@
  * template.php
  */
 
+/**
+ * Implements hook_preprocess_page()
+ * Remove "container" class form navbar
+ */
 function sod3_bootstrap_preprocess_page(&$variables) {
   $variables['navbar_classes_array'] = array('navbar', 'navbar-default');
 }
