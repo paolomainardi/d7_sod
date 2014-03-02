9d05638ffda4358251d8b6eea18f09aa650cda76
Paolo Mainardi <paolo@twinbit.it>
2014-03-02 18:37:52 +0100

    add flag link as an extra field, style it as a bootstrap button

--- sites/all/themes/sod3_bootstrap/template.php
+++ sites/all/themes/sod3_bootstrap/template.php
@@ -12,3 +12,26 @@
 function sod3_bootstrap_preprocess_page(&$variables) {
   $variables['navbar_classes_array'] = array('navbar', 'navbar-default');
 }
+
+/**
+ *
+ */
+function sod3_bootstrap_preprocess_flag(&$vars) {
+  $flag =& $vars['flag'];
+  $action = $vars['action'];
+  $entity_id = $vars['entity_id'];
+
+  if ($flag->name == 'partecipo') {
+    $classes = &$vars['flag_wrapper_classes_array'];
+    $classes[] = 'btn';
+
+    // use the red button
+    if ($action == 'unflag') {
+      $classes[] = 'btn-warning';
+    }
+    // use the blue button
+    if ($action == 'flag') {
+      $classes[] = 'btn-primary';
+    }
+  }
+}
